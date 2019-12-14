0b595de43be0d3cc2a5d0a79f7d70a2ff9f49a6f
XuanTien <xuantien1026@gmail.com>
2019-12-14 18:43:30 +0700

    Initial code for trade-in

--- wp-content/themes/9team/functions.php
+++ wp-content/themes/9team/functions.php
@@ -61,3 +61,103 @@ function my_custom_sizes( $sizes ) {
         'homepage_slide_small_m' => __('Ảnh Slide nhỏ cho mobile'),
     ) );
 }
+
+function create_tcdm() {
+ 
+    $label = array(
+        'name' => 'Thu cũ đổi mới',
+        'singular_name' => 'Sản phẩm cũ'
+    );
+
+    $args = array(
+        'labels' => $label,
+        'description' => 'Post type thu cũ đổi mới điện thoại',
+        'supports' => array(
+            'title',
+            'author',
+            'thumbnail',
+        ),
+        'taxonomies' => array( 'category-tcmd' ),
+        'hierarchical' => false,
+        'public' => true,
+        'show_ui' => true,
+        'show_in_menu' => true,
+        'show_in_nav_menus' => true,
+        'show_in_admin_bar' => true,
+        'menu_position' => 5,
+        'menu_icon' => '',
+        'can_export' => true,
+        'has_archive' => true,
+        'exclude_from_search' => false,
+        'publicly_queryable' => true,
+        'capability_type' => 'post'
+    );
+
+    register_post_type( 'tcdm' , $args );
+
+}
+
+add_action( 'init', 'create_tcdm' );
+
+function create_taxonomy_tcdm() {
+ 
+    $labels = array(
+        'name' => 'Brands',
+        'singular' => 'Brand',
+        'menu_name' => 'Brand'
+    );
+
+    $args = array(
+        'labels'                     => $labels,
+        'hierarchical'               => true,
+        'public'                     => true,
+        'show_ui'                    => true,
+        'show_admin_column'          => true,
+        'show_in_nav_menus'          => true,
+        'show_tagcloud'              => false,
+    );
+
+    register_taxonomy('category-tcdm', 'tcdm', $args);
+
+}
+
+// Hook into the 'init' action
+add_action( 'init', 'create_taxonomy_tcdm', 0 );
+
+
+
+function test_wp() {
+ 
+    $args = array(
+        'post_type' => 'tcdm',
+    );
+    $the_query = new WP_Query( $args );
+
+    // echo "<pre>";
+    // print_r($the_query);
+    // echo "</pre>";
+    // exit();
+    // // Reset Post Data
+
+    if ( $the_query->have_posts() ) :
+        while ( $the_query->have_posts() ) : $the_query->the_post();
+            echo "ID: ".get_the_ID()."\n";
+            // echo "<pre>";
+            // var_dump(get_the_terms( (int)get_the_ID(), 'category-tcdm' ));
+            // exit();
+            $_terms = get_the_terms( (int)get_the_ID(), 'category-tcdm' );
+            foreach ( $_terms as $_term ) {
+                echo "Term: ". $_term->name."\n";
+            }
+        endwhile;
+    endif;
+        
+    // foreach($the_query as $_item) {
+    //     echo $_item->ID;
+    // }
+
+    wp_reset_postdata();
+
+    // return $the_query;
+}
+add_shortcode('tcdm', 'test_wp');
